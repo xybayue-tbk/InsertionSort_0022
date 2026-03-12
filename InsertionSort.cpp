0b6aabad0efb81e58146de594639163dc1bbbabd
xybayue-tbk <xybayue@gmail.com>
2026-03-12 15:20:09 +0700

    menampilkan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -53,3 +53,17 @@ void insertionsort()
         }
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
